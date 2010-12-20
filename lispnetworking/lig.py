2b9d9d47de1c15ae074f677655e80d78004c4cf1
Job W.J. Snijders <job@instituut.net>
2010-12-20 16:49:51 +0100

    first steps at creating a lig in python

---
+++ lispnetworking/lig.py
@@ -0,0 +1,23 @@
+#!/opt/local/bin/python2.5
+
+# from twisted.internet.protocol import DatagramProtocol
+# from twisted.internet import reactor
+
+from lispnetworking import packet
+import sys
+import pprint
+import argparse
+
+def main():
+	parser = argparse.ArgumentParser(description='LISP Internet Gropher.')
+	parser.add_argument('
+
+
+# mapresolver_ip_or_name
+# eid prefix
+# lcaf instanceid
+# authentication_key
+# debug
+
+if __name__ == "__main__":
+    main()            
