12640f277c2cf21c27976a9915a3edc0baa22f5d
Job W.J. Snijders <job@instituut.net>
2010-12-21 08:18:30 +0100

    lig is still broken, seems hard to build constructs

--- lispnetworking/lig.py
+++ lispnetworking/lig.py
@@ -32,15 +32,58 @@ def main():
 		parser.error("Please specify an EID address")
 	eid_address = args[0]
 	print eid_address
-	
-	
-	data = Container(destination = eid_address)
-	
-	p = Container()
+		
+	p = Container()	
 	p.type = 'encapcontrol'
-	p.data = Container()
+	p.data = Container(
+	ip_header = Container(
+		data = Container(
+			checksum = 20513,
+			destination = '153.16.0.0',
+			flags = Container(dont_fragment = False, more_fragments = False),
+			frame_offset = 0,
+			header_length = 20,
+			identification = 54321,
+			options = '', 
+			payload_length = 36, 
+			protocol = 'UDP', 
+			source = '172.16.42.205', 
+			tos = Container(high_reliability = False, high_throuput = False, minimize_cost = False, minimize_delay = False, precedence = 0),
+			total_length = 56, 
+			ttl = 255, 
+			version = 4
+			),
+		type = 'IPv4'
+	), 
+	lisp_control_message = Container(
+		authoritive = False, 
+		eid_mask_len = 32, 
+		eid_prefix = '153.16.0.0', 
+		eid_prefix_afi = 'IPv4', 
+		itr_rloc_address = ['172.16.42.205'], 
+		itr_rloc_afi = 'IPv4', 
+		itr_rloc_count = 1, 
+		map_record = None, 
+		map_reply_record = False, 
+		nonce = '\x0f~o\xdcNxq\t', 
+		probe = False, 
+		record_count = 1, 
+		send_map_request = False, 
+		source_eid_address = None, 
+		source_eid_afi = 'zero', 
+		type = 'maprequest'
+	), 
+	type_inner_header = 'maprequest',
+	type_outer_header = 'encapcontrol',
+	udp_header = Container(
+		checksum = 59496,
+		destination = 4342, 
+		header_length = 8, 
+		payload_length = 28, 
+		source = 55147)
+	)
 
-#	Container()
+#	p.data = Container()
 	
 #	p.data.ip_header = Container()
 #	p.data.ip_header.type = 'IPv4',
@@ -50,9 +93,9 @@ def main():
 #	p.data.ip_header.data.identification = '12345',
 #	p.data.ip_header.data.protocol = 'UDP',
 #	p.data.ip_header.data.source = options.source
-
+	pprint.pprint(p.type)
 	payload = packet.structure.build(p)
-	
+	pprint.pprint(payload)
 		
 	
 
